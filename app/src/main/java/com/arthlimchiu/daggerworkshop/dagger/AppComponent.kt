e9b6cc45fb1a0ff29881f28846c5fde9008c0041
Arth Limchiu <limchiuarth@gmail.com>
2019-09-10 20:23:32 +0800

    Removed inject method

--- app/src/main/java/com/arthlimchiu/daggerworkshop/dagger/AppComponent.kt
+++ app/src/main/java/com/arthlimchiu/daggerworkshop/dagger/AppComponent.kt
@@ -1,10 +1,8 @@
 package com.arthlimchiu.daggerworkshop.dagger
 
-import com.arthlimchiu.daggerworkshop.userdetails.UserDetailsActivity
 import dagger.Component
 
 @Component(modules = [AppModule::class])
 interface AppComponent {
-
-    fun inject(activity: UserDetailsActivity)
+    
 }
